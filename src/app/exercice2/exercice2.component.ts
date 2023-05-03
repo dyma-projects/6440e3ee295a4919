c244c32cfc4f4a3b3cf172f6027de50d888cecc1
Jonathan LEVEQUE <jleveque@scheiber.fr>
2023-05-03 10:32:35 +0200

    Chapitre2

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
   templateUrl: './exercice2.component.html',
-  styleUrls: ['./exercice2.component.css']
+  styleUrls: ['./exercice2.component.css'],
 })
-export class Exercice2Component implements OnInit {
+export class Exercice2Component implements OnInit, AfterViewInit {
+  @ViewChild('mySquare') public squareRef: ElementRef<HTMLDivElement>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  ngAfterViewInit(): void {
+    this.squareRef.nativeElement.addEventListener('click', () => {
+      console.log('click');
+    });
   }
-
 }
